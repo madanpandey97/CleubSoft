bf23f402b11ef70ae589b0cd747210935a665fca
madanpandey97 <madanpandey97@gmail.com>
2017-04-20 01:32:46 +0530

    wsgi file changed

--- cleubsoft/wsgi.py
+++ cleubsoft/wsgi.py
@@ -7,10 +7,16 @@ For more information on this file, see
 https://docs.djangoproject.com/en/1.11/howto/deployment/wsgi/
 """
 
+
 import os
+import sys
+
+path = '/home/pythonanywhereusername/cleubsoft'  # use your own PythonAnywhere username here
+if path not in sys.path:
+    sys.path.append(path)
+
+os.environ['DJANGO_SETTINGS_MODULE'] = 'cleubsoft.settings'
 
 from django.core.wsgi import get_wsgi_application
-
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "cleubsoft.settings")
-
-application = get_wsgi_application()
+from django.contrib.staticfiles.handlers import StaticFilesHandler
+application = StaticFilesHandler(get_wsgi_application())
